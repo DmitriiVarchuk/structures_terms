85a244de17f504b2d69231d0dd8fd14e3855d441
Dmytro Varchuk <loltop792@gmail.com>
2025-02-07 17:50:52 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Point {
+	int x;
+	int y;
+
+	void distance(Point p) {
+		cout << "Distance between points: " << sqrt(pow(p.x - x, 2) + pow(p.y - y, 2)) << endl;
+	}
+};
+
+int main() {
+	Point p1, p2;
+	cout << "Enter x and y coordinates of point 1: ";
+	cin >> p1.x >> p1.y;
+	cout << "Enter x and y coordinates of point 2: ";
+	cin >> p2.x >> p2.y;
+	p1.distance(p2);
+
+
+	return 0;
+}
+
